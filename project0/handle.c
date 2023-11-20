5efe472148f578b462e9fce6f6456549a63f2a99
lanand <lanand@indeed.com>
2023-11-19 19:24:06 -0600

    Implement Signal Handling

--- project0/handle.c
+++ project0/handle.c
@@ -7,6 +7,13 @@
 #include <unistd.h>
 #include "util.h"
 
+void signal_handler(int signal) {
+    ssize_t bytes;
+    const int STDOUT = 1;
+    bytes = write(STDOUT, "Nice try.\n", 10);
+    if(bytes != 10)
+        exit(-999);
+}
 
 /*
  * First, print out the process ID of this process.
@@ -19,7 +26,26 @@
  */
 int main(int argc, char **argv)
 {
-  return 0;
+    pid_t current_pid = getpid();
+    printf("%d\n", current_pid);
+    struct sigaction sig_handler_info = {signal_handler, 0, SA_RESTART};
+
+    sigaction(SIGINT, &sig_handler_info, NULL);
+    struct timespec rqtp = {1, 0};
+    struct timespec rmtp;
+    int nano_status = 0;
+
+    while(1) {
+        if (nano_status == EINTR) {
+            rqtp = rmtp;
+        } else {
+            printf("Still here\n");
+            rqtp.tv_sec = 1;
+            rqtp.tv_nsec = 0;
+        }
+        nano_status = nanosleep(&rqtp, &rmtp);
+    }
+    return 0;
 }
 
 
