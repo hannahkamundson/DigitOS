a8fd83f43639d073cdba7b13c734b4b4256e612b
hannahkamundson <hannahkamundson@gmail.com>
2024-01-06 13:05:51 -0600

    Working through the shell + deleting vs code + adding to gitignore

--- project0/utcsh.c
+++ project0/utcsh.c
@@ -9,11 +9,13 @@ in the future */
 #include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /* Global variables */
 /* The array for holding shell paths. Can be edited by the functions in util.c*/
 char shell_paths[MAX_ENTRIES_IN_SHELLPATH][MAX_CHARS_PER_CMDLINE];
-static char prompt[] = "utcsh> "; /* Command line prompt */
+static char prompt[] = "DigitOS> "; /* Command line prompt */
 static char *default_shell_path[2] = {"/bin", NULL};
 /* End Global Variables */
 
@@ -25,12 +27,38 @@ struct Command {
 };
 
 /* Here are the functions we recommend you implement */
-
+struct Command readUserInput();
+char *readRaw();
 char **tokenize_command_line(char *cmdline);
 struct Command parse_command(char **tokens);
 void eval(struct Command *cmd);
 int try_exec_builtin(struct Command *cmd);
 void exec_external_cmd(struct Command *cmd);
+
+/*
+remembering pointers:
+
+int number = 1;
+printf("NUMBER: %d", number);
+  utcsh> NUMBER: 1
+printf("\nNUMBER&: %p", &number);
+  utcsh> NUMBER&: 0x7ffec8a2660c
+
+int* numberPointer = &number;
+printf("\nPOINTER: %p", numberPointer);
+  utcsh> POINTER: 0x7ffec8a2660c
+printf("\nPOINTER*: %d", *numberPointer);
+  utcsh> POINTER*: 1
+printf("\nPOINTER POINTER: %p", &numberPointer);
+  utcsh> POINTER POINTER: 0x7ffec8a26610% 
+
+char* input = "This is my input";
+printf("INPUT: %s", input);
+  utcsh> INPUT: This is my input
+printf("INPUT POINTER: %p", input);
+  utcsh> INPUT POINTER: 0x56299a6d9004
+
+*/
 
 /* Main REPL: read, evaluate, and print. This function should remain relatively
    short: if it grows beyond 60 lines, you're doing too much in main() and
@@ -43,21 +71,42 @@ int main(int argc, char **argv) {
   (void)argc;
   (void)argv;
 
-  while (1) {
-    printf("%s", prompt);
 
+  // while (1) {
+    printf("%s", prompt);
+    
     /* Read */
+    struct Command command = readUserInput();
 
     /* Evaluate */
+    eval(&command);
 
     /* Print (optional) */
-  }
+  // }
   return 0;
 }
 
-/* NOTE: In the skeleton code, all function bodies below this line are dummy
-implementations made to avoid warnings. You should delete them and replace them
-with your own implementation. */
+struct Command readUserInput() {
+  // Read the raw input
+  char* rawInput = readRaw();
+  printf("\nRAW: %s", rawInput);
+
+  // Tokenize it and get it in a string array
+  char** tokenizedInput = tokenize_command_line(rawInput);
+  int numberOfArgs = (int) sizeof(tokenizedInput) / sizeof(char*);
+
+  for (int i = 0; i < numberOfArgs; i++) {
+    printf("\nTOKENIZED %d: %s", i, tokenizedInput[i]);
+  }
+
+  return parse_command(tokenizedInput);
+}
+
+char* readRaw() {
+  char* input = "path";
+
+  return input;
+}
 
 /** Turn a command line into tokens with strtok
  *
@@ -66,8 +115,24 @@ with your own implementation. */
  * have, then allocate a char** of sufficient size and fill it using strtok()
  */
 char **tokenize_command_line(char *cmdline) {
+  // TODO
   (void)cmdline;
-  return NULL;
+
+  int numberOfArgs = 2;
+
+  // Get the array of strings (char*)
+  char* localArray[] = { "cd", "examples" };
+
+  // This holds the address of the array of strings we can actually return
+  char** returnable = malloc(numberOfArgs * sizeof(char*));
+
+  // For each argument, move it to the returnable
+  for (int i = 0; i < numberOfArgs; i++) {
+    returnable[i] = malloc(sizeof(localArray[i]) * sizeof(char));
+    strcpy(returnable[i], localArray[i]);
+  }
+  
+  return returnable;
 }
 
 /** Turn tokens into a command.
@@ -78,6 +143,7 @@ char **tokenize_command_line(char *cmdline) {
  * Command.
  */
 struct Command parse_command(char **tokens) {
+  // TODO
   struct Command dummy = {.args = tokens, .outputFile = NULL};
   return dummy;
 }
@@ -88,8 +154,29 @@ struct Command parse_command(char **tokens) {
  * should work out what the correct type is and take the appropriate action.
  */
 void eval(struct Command *cmd) {
-  (void)cmd;
+  // If it wasn't built in, 
+  if (try_exec_builtin(cmd) == 0) {
+    // Run it as an external command
+    exec_external_cmd(cmd);
+  }
+
   return;
+}
+
+int strings_same(char* string1, char* string2) {
+  if (strcmp(string1, string2) == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void writeError(char msg[]) {
+  int nbytes_written = write(STDERR_FILENO, msg, strlen(msg));
+
+  if(nbytes_written != (int) strlen(msg)){
+    exit(2);  // Shouldn't really happen -- if it does, error is unrecoverable
+  }
 }
 
 /** Execute built-in commands
@@ -98,7 +185,37 @@ void eval(struct Command *cmd) {
  * If the command is not a built-in command, do nothing and return 0
  */
 int try_exec_builtin(struct Command *cmd) {
-  (void)cmd;
+  char* program = cmd->args[0];
+  printf("\nSearching for command: %s", program);
+
+  // Try to match the string
+  if (strings_same("exit", program)) {
+    printf("\n[exit] command executing.");
+    exit(0);
+  } else if (strings_same("cd", program)) {
+    printf("\n[cd] command executing.");
+
+    int numberOfArgs = (int) sizeof(cmd->args) / sizeof(char*);
+
+    if (numberOfArgs != 2) {
+      writeError("You must have exactly one directory.\n");
+    }
+
+    int result = chdir(cmd->args[1]);
+
+    if (result != 0) {
+      writeError("chdir had an error");
+    }
+
+    return 1;
+  } else if (strings_same("path", program)) {
+    printf("\n[path] command executing.");
+    return 1;
+  }
+
+  // Otherwise return 0 so we can attempt to execute it externally
+  printf("\nNo build in command found.");
+
   return 0;
 }
 
